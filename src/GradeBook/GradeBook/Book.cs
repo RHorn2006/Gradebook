fd395342102c9fc7dcaedf34b93c21393e66952b
RHorn <ryan.horn@bglgroup.co.uk>
2023-11-30 17:05:55 +0000

    Initial set up

---
+++ src/GradeBook/GradeBook/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace GradeBook;
+class Book
+{
+    public Book()
+    {
+        grades = new List<double>();
+    }
+    List<double> grades;
+    public void AddGrade(double grade)
+    {
+        grades.Add(grade);
+    }
+}
---
+++ src/GradeBook/GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Emit;
+
+namespace GradeBook;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var book = new Book();
+        book.AddGrade(67.5);
+        book.AddGrade(45.7);
+
+        List<double> grades = new List<double>() { 64.5, 45.3, 62.6 };
+        grades.Add(56.1);
+        
+        var result = 0.0;
+        foreach (double number in grades)
+        {
+            result += number;
+        }
+        result /= grades.Count;
+        Console.WriteLine($"The average grade is {result:0.00000}");
+    }
+}
